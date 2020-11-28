d5d251e12da5b27986044a56cea6d7514d1e84b9
rikuru_to <usaginosippo865@gmail.com>
2020-11-28 14:57:19 +0900

    不要な使用を削除します

--- BackGroundMovie/Program.cs
+++ BackGroundMovie/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
-using System.Drawing;
 using System.Windows.Forms;
 
 namespace BackGroundMovie
